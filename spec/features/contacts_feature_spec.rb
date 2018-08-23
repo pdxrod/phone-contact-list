ef467559a86951480281fae2328ae73f48216b3b
z <x@y.com>
2018-08-23 08:13:16 -0700

    Tests pass

--- spec/features/contacts_feature_spec.rb
+++ spec/features/contacts_feature_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe ContactsController, type: :feature do
   describe "testing data" do
     it "should have the right numbers for the right names" do
 # To test the app is searching using both numbers and letters - e.g. number
-# 1 also means abc, number 9 also means wxyz. Hitting 1 should show
+# 2 also means abc, number 9 also means wxyz. Hitting 2 should show
 # Alan 00, because Alan has 'a' in his name. Hitting 4 should show Ashok 44,
-# because he has 4 in his number. Hitting 9 should show nobody.
+# because he has 4 in his number.
     alan = Contact.find_by_name( "Alan" )
     expect(alan.number).to eq "00"
     ashok = Contact.find_by_name( "Ashok" )
@@ -32,22 +32,36 @@ RSpec.describe ContactsController, type: :feature do
   end
 
   describe "going to a number" do
-    it "should show contact with an a in his name if you hit 1" do
-      visit "/1"
+    it "should show contacts with a in their names if you hit 2" do
+      visit "/2"
       expect(page).to have_content "Alan"
-      expect(page).not_to have_content "Ashok"
+      expect(page).to have_content "Ashok"
+      expect(page).to have_content "Nayani"
+      expect(page).to have_content "Jane"
+      expect(page).not_to have_content "Jim"
     end
 
     it "should show contact with a 4 in his number if you hit 4" do
       visit "/4"
       expect(page).to have_content "Ashok"
-      expect(page).not_to have_content "Alan"
     end
 
-    it "should not show either one if you hit 9" do
-      visit "/9"
+    it "should only show Jim 11 if you hit 1" do
+      visit "/1"
+      expect(page).to have_content "Jim"
       expect(page).not_to have_content "Alan"
       expect(page).not_to have_content "Ashok"
+      expect(page).not_to have_content "Nayani"
+      expect(page).not_to have_content "Jane"
+    end
+
+    it "should only show Nayani 33 if you hit 9" do
+      visit "/9"
+      expect(page).to have_content "Nayani"
+      expect(page).not_to have_content "Alan"
+      expect(page).not_to have_content "Ashok"
+      expect(page).not_to have_content "Jane"
+      expect(page).not_to have_content "Jim"
     end
   end
 
