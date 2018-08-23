c0b106941514b08eed68d6b9cb66406bad0e9fd1
z <x@y.com>
2018-08-23 07:59:03 -0700

    Getting closer - ContactsController associating number with letters, e.g. 2 with 'abc', and finding contacts with either the number or one of the corresponding letters in his or her number or name

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,32 @@
 class ContactsController < ApplicationController
 
   def index
-
-# params["number"]
-
-    @contacts = Contact.all
+    @contacts = contacts_for_number params["number"]
     respond_to do |format|
       format.html
       format.json { render json: @contacts }
     end
   end
+
+  private
+    LETTERS = ["~", "~", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+    def contacts_for_number( number )
+      return [] if number.nil? 
+      letters = LETTERS[ number ]
+      contacts = []
+      Contact.all.each do |contact|
+        contacts << contact if contact.number.include?( number.to_s ) || contains_letter?( contact.name, letters )
+      end
+      return contacts
+    end
+
+    def contains_letter?( name, string )
+      letters = string.split ''
+      lower = name.downcase
+      letters.each do |letter|
+        return true if lower.include? letter
+      end
+      return false
+    end
 
 end
