da75609e8a1bbd828635792c46f588f4068203eb
z <x@y.com>
2018-08-22 19:40:35 -0700

    New failure - Contact has no name or number attribute

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @contact = Contact.new(name: "Fred Bloggs", number: "2021113333")
+  end
+
+  describe "contact" do
+    it "creates a contact" do
+      expect(@contact.name).to eq "Fred Bloggs"
+      expect(@contact.number).to eq "2021113333"
+    end
+  end
+
 end
